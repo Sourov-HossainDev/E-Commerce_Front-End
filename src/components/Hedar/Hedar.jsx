ab553b4252040cf9981448c9560e5ecac08463a7
Sourov-HossainDev <sourovhossain2326@gmail.com>
2024-05-13 00:21:59 +0600

    dorp Down

--- src/components/Hedar/Hedar.jsx
+++ src/components/Hedar/Hedar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Container from '../Lay/Container'
 import Flex from '../Navbar/Flex/Flex'
 import Search from '../Lay/Search'
@@ -6,21 +6,63 @@ import { ImSearch } from "react-icons/im";
 import { MdPerson } from "react-icons/md";
 import { MdOutlineArrowDropDown } from "react-icons/md";
 import { FaShoppingCart } from "react-icons/fa";
+import DropDown from '../Lay/DropDown';
 
 
 const Hedar = () => {
+    const ref = useRef();
+    const [show, setShow] =useState(false);
+
+    
+    useEffect(()=>{
+        document.body.addEventListener('click', (e)=>{
+            if(ref.current.contains(e.target)){
+                setShow(true)
+            }else{
+                setShow(false)
+            }
+        })
+    },[])
+
+    // const handelClick = ()=>{
+    //     setShow(!show)
+    // }
     return (
         <div className='bg-[#F5F5F3] '>
             <Container className="py-10 ">
 
                 <Flex className="justify-between">
                     <div className='flex items-center gap-x-2.5  '>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="10" viewBox="0 0 19 10" fill="none" className='text-xl'>
-                            <path d="M0.653061 2.93878H17.6327C17.9933 2.93878 18.2857 2.64641 18.2857 2.28571V0.653061C18.2857 0.292367 17.9933 0 17.6327 0H0.653061C0.292367 0 0 0.292367 0 0.653061V2.28571C0 2.64641 0.292367 2.93878 0.653061 2.93878Z" fill="#262626" />
-                            <path d="M0.5 9.46929H13.5C13.7762 9.46929 14 9.17693 14 8.81623V7.18358C14 6.82288 13.7762 6.53052 13.5 6.53052H0.5C0.223844 6.53052 0 6.82288 0 7.18358V8.81623C0 9.17693 0.223844 9.46929 0.5 9.46929Z" fill="#262626" />
-                        </svg>
-                        <p className='font-DMsans text-[14px]'>Shop by Category</p>
+
+                        <DropDown dropRef={ref} className='cursor-pointer' >
+
+                            <div  className='flex items-center gap-x-2.5 text-[14px] font-DMsans font-bold '>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="19" height="10" viewBox="0 0 19 10" fill="none" className='text-xl'>
+                                    <path d="M0.653061 2.93878H17.6327C17.9933 2.93878 18.2857 2.64641 18.2857 2.28571V0.653061C18.2857 0.292367 17.9933 0 17.6327 0H0.653061C0.292367 0 0 0.292367 0 0.653061V2.28571C0 2.64641 0.292367 2.93878 0.653061 2.93878Z" fill="#262626" />
+                                    <path d="M0.5 9.46929H13.5C13.7762 9.46929 14 9.17693 14 8.81623V7.18358C14 6.82288 13.7762 6.53052 13.5 6.53052H0.5C0.223844 6.53052 0 6.82288 0 7.18358V8.81623C0 9.17693 0.223844 9.46929 0.5 9.46929Z" fill="#262626" />
+                                </svg>
+                                Shop by Category
+
+
+                            </div>
+                            {
+                                show &&
+                                <ul className='absolute '>
+                                <li>home</li>
+                                <li>home</li>
+                                <li>home</li>
+                                <li>home</li>
+                                <li>home</li>
+                            </ul>
+                            }
+                            
+
+
+                        </DropDown>
                     </div>
+
+
+
                     <div className='relative'>
                         <Search className="py-4 px-5 w-[601px] outline-none " placeholder="Search Products" />
                         <ImSearch className='absolute top-5 right-8 text-xl font-Dmsans' />
